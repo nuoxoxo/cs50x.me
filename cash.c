f0195e34b0bb53f214ffeded3901db8adecbe916
malcolmnuoxu <49533950+malcolmnuoxu@users.noreply.github.com>
2021-02-20 14:18:43 +0100

    Update cash.c

--- cash.c
+++ cash.c
@@ -4,7 +4,6 @@
 
 int main(void)
 {
-
     float n;
 
     do
@@ -14,15 +13,15 @@ int main(void)
     }
     while (n < 0);
 
+    // int tt = qt + dm + nk + ct;
     int ct = round(n * 100); // debugged
     int qt = 0;
     int dm = 0;
-    int nk = 0;
-    // int tt = qt + dm + nk + ct;
+    int nk = 0;    
 
     // calculate quarter coins first
     qt = round(ct / 25);
-    // use module ie. "ct modulo 25" to replace "ct = ct - qt * 25"
+    // use modulo to replace "ct = ct - qt * 25"
     ct = ct % 25;
 
     if (ct == 0)
@@ -31,7 +30,7 @@ int main(void)
     }
     else
     {
-        // then calculate dimes
+        // calculate dimes
         dm = round(ct / 10);
         ct = ct % 10;
 
