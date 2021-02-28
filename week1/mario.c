5ac0e8ed6a37f42d2f302969a324dbbd4bba03c3
malcolmnuoxu <49533950+malcolmnuoxu@users.noreply.github.com>
2021-02-28 18:34:04 +0100

    Update mario.c

--- week1/mario.c
+++ week1/mario.c
@@ -1,52 +1,31 @@
 #include <cs50.h>
 #include <stdio.h>
 
-// re-do on Jan27, orginally dropped on Jan15
-
-int main(void)
-{
+int main(void){
     int n;
 
-    do
-    {
+    do{
         n = get_int("Height: ");
     }
-    while (n < 1 || n > 8);
+    while (n < 1 || n > 36);
 
-    // Note (Jan27) :: change j to decrement
-    // also add an increment k
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = n - 1; j >= 0; j--)
-        {
-            if (j > i) // OR if ( j >= i + 1)
-            {
+    for (int i = 0; i < n; i++){
+        for (int j = n - 1; j >= 0; j--){
+            if (j > i) {
                 printf(" ");
-                
             }
-            else
-            {
+            else{
                 printf("#");
             }
-            
-        
-
         }
-        
+
         printf("  ");
-        
-        for (int k = 0; k < n; k++) 
-        {
-            if (k < i + 1)
-            {
+
+        for (int k = 0; k < n; k++) {
+            if (k < i + 1){
                 printf("#");
-                
             }
-            
-        } 
+        }
         printf("\n");
-
     }
-    
-
 }
