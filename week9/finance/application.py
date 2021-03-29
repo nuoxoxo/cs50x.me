7f099afe8c349d08f6d890eb4b991629f2a69ae2
nuo.o <49533950+nuoxoxo@users.noreply.github.com>
2021-03-30 00:10:57 +0200

    Add files via upload

--- week9/finance/application.py
+++ week9/finance/application.py
@@ -46,19 +46,86 @@ if not os.environ.get("API_KEY"):
 @login_required
 def index():
     """Show portfolio of stocks"""
-    return render_template("index.html")
 
-@app.route("/buy", methods=["GET", "POST"])
-@login_required
-def buy():
-    """Buy shares of stock"""
-    return apology("TODO")
+
+    return render_template("index.html")
 
 
 @app.route("/history")
 @login_required
 def history():
     """Show history of transactions"""
+
+    history = db.execute("SELECT symbol AS Symbol, quantity AS Shares, quote AS Price \ FROM history WHERE u_id = ?", session['user_id'])
+
+    return render_template("history.html", history=history)
+
+
+@app.route("/buy", methods=["GET", "POST"])
+@login_required
+def buy():
+    """Buy shares of stock"""
+
+    if request.method == "GET":
+        return render_template("buy.html")
+
+    if request.method == "POST":
+
+        if not request.form.get("shares"):
+            return apology("Enter the number of shares you want")
+
+        ### :( buy handles fractional, negative, and non-numeric shares
+        ### todo :: check if shares <= 0
+        ### todo :: check type of shares input == num
+
+
+        ### define quote
+        if not request.form.get("symbol"):
+            return apology("Enter the stock name.")
+        else:
+            quote = lookup(request.form.get("symbol"))
+
+        if quote == None:
+            return apology("Stock name not found")
+
+        ### define shares, number_of_shares, symbol
+        shares = request.form.get("shares")
+        symbol = request.form.get("symbol").upper()
+        num_shares = int(shares)
+
+        ### check price
+        price = quote.get("price")
+
+        ### check balance
+        cash = db.execute("SELECT cash FROM users WHERE id = ?", session['user_id'])
+        balance = cash[0]["cash"]
+
+        ### not enough cash: apology
+        ### else: 1. buy
+        ### 2. calculate new balance
+
+        total = price * num_shares
+
+        if total > balance:
+            return apology("Not enough cash.")
+        else:
+            balance -= total
+            db.execute("INSERT INTO history (symbol, quantity, quote, total, u_id) VALUES(?, ?, ?, ?, ?)", symbol, num_shares, price, total, session["user_id"])
+            db.execute("UPDATE users SET cash = (?) WHERE id = (?)", round(balance, 2), session["user_id"])
+
+            ### redirect to home
+            return redirect("/")
+
+
+
+
+    return apology("TODO")
+
+
+@app.route("/sell", methods=["GET", "POST"])
+@login_required
+def sell():
+    """Sell shares of stock"""
     return apology("TODO")
 
 
@@ -74,18 +141,20 @@ def login():
 
         # Ensure username was submitted
         if not request.form.get("username"):
-            return apology("must provide username", 403)
+            return apology("must provide username")
 
         # Ensure password was submitted
         elif not request.form.get("password"):
-            return apology("must provide password", 403)
+            return apology("must provide password")
 
         # Query database for username
-        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+        rows = db.execute("SELECT * FROM users WHERE username = ?",
+        request.form.get("username"))
 
         # Ensure username exists and password is correct
-        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
-            return apology("invalid username and/or password", 403)
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"],
+        request.form.get("password")):
+            return apology("invalid username and/or password")
 
         # Remember which user has logged in
         session["user_id"] = rows[0]["id"]
@@ -133,7 +202,6 @@ def quote():
             symbol = quote.get("symbol")
             price = quote.get("price")
             return render_template("quoted.html", symbol=synbol, price=usd(price))
-    # return apology("TODO")
 
 
 @app.route("/register", methods=["GET", "POST"])
@@ -151,6 +219,10 @@ def register():
             return apology("Passwords don't match.")
 
         else:
+            ## INSERT the new user into users:
+            ## store a hash of the password, not the password itself
+            ## Hash the user’s password with generate_password_hash
+
             #check_username = len(db.execute("SELECT * FROM users WHERE username = {username}", username=request.form.get("username")))
             if len(db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))) != 0:
                 return apology("This username already taken. Please choose another one.")
@@ -160,14 +232,8 @@ def register():
                 session["user_id"] = uid
                 flash("registered successfully.")
                 return redirect("/")
+
     return render_template("register.html")
-
-
-@app.route("/sell", methods=["GET", "POST"])
-@login_required
-def sell():
-    """Sell shares of stock"""
-    return apology("TODO")
 
 
 def errorhandler(e):
